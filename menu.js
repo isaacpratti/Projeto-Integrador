5a640baed35a94948ad0032fcc5759ccc5491b22
ceciliadumer <163332009+ceciliadumer@users.noreply.github.com>
2024-03-17 16:07:07 -0300

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,4 @@
+var btComecar = document.getElementById("btComecar");
+var btBasico = document.getElementById("btBasico");
+var btIntermediario = document.getElementById("btIntermediario");
+var btAvancado = document.getElementById("btAvancado");
