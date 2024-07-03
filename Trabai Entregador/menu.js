9535a80685361e6b50d56c159aad89068e2e5eae
isaacpratti <163330203+isaacpratti@users.noreply.github.com>
2024-07-03 14:06:56 -0300

    Update menu.js

--- Trabai Entregador/menu.js
+++ Trabai Entregador/menu.js
@@ -2,4 +2,4 @@ const btComecar = document.getElementById("btComecar");
 const btBasico = document.getElementById("btBasico");
 const btIntermediario = document.getElementById("btIntermediario");
 const btAvancado = document.getElementById("btAvancado");
-
+const btHorarios = document.getElementById("btHorarios")
