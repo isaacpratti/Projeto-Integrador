99cc77ee6aa84d13780aa1e8614f5e9e1d450856
isaacpratti <163330203+isaacpratti@users.noreply.github.com>
2024-09-25 12:45:33 -0300

    Delete menu.js

--- menu.js
+++
@@ -1,5 +0,0 @@
-const btComecar = document.getElementById("btComecar");
-const btBasico = document.getElementById("btBasico");
-const btIntermediario = document.getElementById("btIntermediario");
-const btAvancado = document.getElementById("btAvancado");
-
